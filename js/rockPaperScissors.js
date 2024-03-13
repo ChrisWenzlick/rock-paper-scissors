7f9e3f7c9e461aacb814f40c9f79e73052f8dd2d
ChrisWenzlick <cwenzlick01@gmail.com>
2024-03-13 00:11:44 -0500

    Add logic for one game round

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -9,3 +9,31 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return `DRAW: you both selected ${playerSelection}.`;
+    }
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return `YOU LOSE: paper covers rock.`;
+        } else {
+            return `YOU WIN: rock beats scissors.`;
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return `YOU LOSE: scissors cut paper.`;
+        } else {
+            return `YOU WIN: paper covers rock.`;
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return `YOU LOSE: rock beats scissors.`;
+        } else {
+            return `YOU WIN: scissors cut paper.`;
+        }
+    }
+}
