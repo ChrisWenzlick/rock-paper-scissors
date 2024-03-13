66b78ca5735c705765b28f75f5f34ab402f19684
ChrisWenzlick <cwenzlick01@gmail.com>
2024-03-13 00:32:25 -0500

    Change playRound function to return a more useful value representing the winner

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -10,32 +10,49 @@ function getComputerChoice() {
     }
 }
 
+/*
+Plays one round of rock paper scissors
+    PARAMETERS:
+        playerSelection - the player's selection, in string form
+        computerSelection - the computer's selection, in string form
+    RETURNS:
+        1 if the player wins, -1 if the player loses, and 0 if
+        there's a draw or invalid player input
+*/
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.trim().toLowerCase();
 
     if (playerSelection == computerSelection) {
-        return `DRAW: you both selected ${playerSelection}.`;
+        console.log(`DRAW: you both selected ${playerSelection}.`);
+        return 0;
     }
 
     if (playerSelection == "rock") {
         if (computerSelection == "paper") {
-            return `YOU LOSE: paper covers rock.`;
+            console.log(`YOU LOSE: paper covers rock.`);
+            return -1;
         } else {
-            return `YOU WIN: rock beats scissors.`;
+            console.log(`YOU WIN: rock beats scissors.`);
+            return 1;
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "scissors") {
-            return `YOU LOSE: scissors cut paper.`;
+            console.log(`YOU LOSE: scissors cut paper.`);
+            return -1;
         } else {
-            return `YOU WIN: paper covers rock.`;
+            console.log(`YOU WIN: paper covers rock.`);
+            return 1;
         }
     } else if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            return `YOU LOSE: rock beats scissors.`;
+            console.log(`YOU LOSE: rock beats scissors.`);
+            return -1;
         } else {
-            return `YOU WIN: scissors cut paper.`;
+            console.log(`YOU WIN: scissors cut paper.`);
+            return 1;
         }
     } else {
-        return "Please enter rock, paper, or scissors."
+        console.log("Please enter rock, paper, or scissors.");
+        return 0;
     }
 }
